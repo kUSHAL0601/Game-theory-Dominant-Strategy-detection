d6b889014e4315c6ddc7276ae44dd71ed793c35d
Debian Live user <user@localhost.localdomain>
2019-03-09 18:39:04 +0000

    Final update

--- ass1.py
+++ ass1.py
@@ -15,8 +15,10 @@ n_actions.pop(0)
 n_actions.pop(len(n_actions)-1)
 n_actions=list(map(int,n_actions))
 n_players=min(n_players,len(n_actions))
+n_actions=[0]+n_actions
 input()
 payoffs=list(map(float,input().split()))
+
 # payoffs_2d=[]
 # for i in range(len(payoffs)//n_players):
 #     payoffs_2d.append([])
@@ -24,12 +26,14 @@ payoffs=list(map(float,input().split()))
         # payoffs_2d[i]/.append(payoffs[n_players*i+j])
 # print(payoffs_2d)
 
-step_size=[0]*(n_players)
-step_size[0]=n_players
-for i in range(1,n_players):
+step_size=[0]*(n_players+1)
+step_size[1]=n_players
+for i in range(2,n_players+1):
         step_size[i]=step_size[i-1]*n_actions[i-1]
-max_len=step_size[-1]*n_actions[-1]
-print(step_size)
+max_len=len(payoffs)
+payoffs=[0]+payoffs
+# print(step_size)
+# print(max_len)
 
 def get_dominant_strategy(player_idx):
     idx_flag={}
@@ -40,21 +44,21 @@ def get_dominant_strategy(player_idx):
         if j not in idx_flag:
             x=j
             action=1
-            while(action<=n_actions[player_idx] and x<max_len):
+            while action<=n_actions[player_idx] and x<=max_len:
                 idx_flag[x]=1
                 ans=max(ans,payoffs[x])
-                x+=step_size[player_idx]
                 action+=1
+                x+=step_size[player_idx]
             x=j
             action=1
-            while(action<=n_actions[player_idx] and x<max_len):
+            while action<=n_actions[player_idx] and x<=max_len:
                 if payoffs[x]==ans:
                         try:
                             action_count[action]+=1
                         except:
                             action_count[action]=1
-                x+=step_size[player_idx]
                 action+=1
+                x+=step_size[player_idx]
     for action in range(1,n_actions[player_idx]+1):
         try:
             if action_count[action]==max_len/(n_players*n_actions[player_idx]):
@@ -65,12 +69,12 @@ def get_dominant_strategy(player_idx):
         print("No dominant strategies for Player",player_idx)
     else:
         print("Dominant strategies for Player",player_idx,*dominant_strats)
-    return dominant_strats,len(action_count)
+    return dominant_strats,action_count
 
 dominant_strategies={}
 action_counts={}
 flag=False
-for p in range(0,n_players):
+for p in range(1,n_players+1):
     dominant_strategies[p],action_counts[p]=get_dominant_strategy(p)
     print(dominant_strategies[p],action_counts[p])
     if not len(dominant_strategies[p]):
